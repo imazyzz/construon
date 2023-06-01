f07e9a57b97792b8a84f1febb4c90565f725c4fb
Felipe <felipeassis16@hotmail.com>
2023-05-31 22:27:43 -0300

    Update 2.0

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+// scroll sections 
+
+let sections = document.querySelectorAll('section');
+let navbar = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top <= offset + height) {
+            navbar.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+
+    })
+}
